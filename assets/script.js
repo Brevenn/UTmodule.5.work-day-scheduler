0e6d09d5d3d1a999226660449a5532ea9fefdf96
Brevenn <Brevenjh@gmail.com>
2022-07-26 19:42:45 -0500

    added media screen sizing adjustments

--- assets/script.js
+++ assets/script.js
@@ -59,16 +59,3 @@ $(document).ready(function() {
     hourTracking();
 })
 
-// var currentHour = Date.now().gethours();
-// $('.time-block').each(function(){
-//     var val = parseInt($(this).prop('id'));
-//     if(val > currentHour && val < currentHour+6){
-//         $(this).css('background-color','Blue');
-//     }else if(val < currentHour && val > currentHour-6){
-//         $(this).css('background-color','Red');
-//     }else if(val === currentHour){
-//         $(this).css('background-color','Green');
-//     }else{
-//         $(this).css('background-color','White');
-//     }
-// });
